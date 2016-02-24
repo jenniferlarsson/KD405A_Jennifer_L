b2b04cf52bbf87cbcf4e1adb5fa1fa7e81ce2828
jenniferlarsson <jennifer.m.larsson@gmail.com>
2016-02-24 19:58:00 +0100

    skanetrafiken

---
+++ Assignment_5/src/se/mah/ke/jenniferlarsson/ClockLogic.java
@@ -0,0 +1,20 @@
+package se.mah.ke.jenniferlarsson;
+
+public class ClockLogic {
+	
+	private DigitalClockGUI digitalClockGUI;
+	private int alarmHour;
+	private int alarmMinute;
+
+	public ClockLogic(DigitalClockGUI digitalClockGUI){
+		this.digitalClockGUI= digitalClockGUI;
+	}
+	
+	public void setAlarm(int hours, int minute){
+		
+	}
+	
+	public void clearAlarm(){
+		
+	}
+}

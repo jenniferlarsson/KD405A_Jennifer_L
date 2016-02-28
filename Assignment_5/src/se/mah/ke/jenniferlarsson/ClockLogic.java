f3b444670f3c82bfd2e7921d4a5ddd7b8074ff88
jenniferlarsson <jennifer.m.larsson@gmail.com>
2016-02-28 13:18:09 +0100

    assignment5

--- Assignment_5/src/se/mah/ke/jenniferlarsson/ClockLogic.java
+++ Assignment_5/src/se/mah/ke/jenniferlarsson/ClockLogic.java
@@ -1,5 +1,8 @@
 package se.mah.ke.jenniferlarsson;
 
+import java.text.DecimalFormat;
+import java.util.Calendar;
+
 public class ClockLogic {
 	
 	private DigitalClockGUI digitalClockGUI;
@@ -8,13 +11,52 @@ public class ClockLogic {
 
 	public ClockLogic(DigitalClockGUI digitalClockGUI){
 		this.digitalClockGUI= digitalClockGUI;
+		new ClockThread().start();
 	}
 	
 	public void setAlarm(int hours, int minute){
-		
+		alarmHour = hours;
+		alarmMinute= minute;
 	}
 	
 	public void clearAlarm(){
+		alarmHour = -1;
+		alarmMinute = -1;
 		
 	}
+private class ClockThread extends Thread {
+		
+		@Override
+		public void run(){
+			
+			DecimalFormat decimalFormat = new DecimalFormat("00");
+			
+			
+			while (true){
+				
+				Calendar calendar= Calendar.getInstance();
+				int hour= calendar.get(Calendar.HOUR_OF_DAY);
+				int minute= calendar.get(Calendar.MINUTE);
+				int second = calendar.get(Calendar.SECOND);
+				
+				digitalClockGUI.setTimeOnLabel(decimalFormat.format(hour)+ ":" + decimalFormat.format(minute) + ":" + decimalFormat.format(second));
+				
+				if(alarmHour == hour && alarmMinute == minute){
+					digitalClockGUI.activateAlarm(true);
+					
+				} else {
+				
+					digitalClockGUI.activateAlarm(false);
+				}
+				try {
+					Thread.sleep(900);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					return;
+				}
+			} 
+}
+
+}
+
 }
--- Assignment_5/src/se/mah/ke/jenniferlarsson/DigitalClockGUI.java
+++ Assignment_5/src/se/mah/ke/jenniferlarsson/DigitalClockGUI.java
@@ -2,16 +2,28 @@ package se.mah.ke.jenniferlarsson;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.ImageIcon;
 
 public class DigitalClockGUI extends JFrame {
 
 	private JPanel contentPane;
 	
 	private ClockLogic clockLogic;
+	private JTextField textFieldMinutes;
+	private JTextField txtFieldHour;
+	private JLabel lblCurrentTime = new JLabel("Time");
+	private JLabel lblAlarmTime = new JLabel("No alarm set");
+	private JLabel lblNewLabel = new JLabel("");
 	
 
 	/**
@@ -34,19 +46,79 @@ public class DigitalClockGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public DigitalClockGUI() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(DigitalClockGUI.class.getResource("/resources/clocklogic.png")));
+		setTitle("Digital clock with alarm");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 519, 299);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		clockLogic= new ClockLogic(this);
+		
+		JButton btnSetAlarm = new JButton("Set Alarm");
+		btnSetAlarm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int hour = Integer.parseInt(txtFieldHour.getText());
+				int minutes = Integer.parseInt(textFieldMinutes.getText());
+				clockLogic.setAlarm(hour, minutes);
+				lblAlarmTime.setText("Alarm at: " + hour + ":" + minutes);
+				
+			}
+		});
+		btnSetAlarm.setBounds(248, 221, 117, 29);
+		contentPane.add(btnSetAlarm);
+		
+		JButton btnClearAlarm = new JButton("Clear Alarm");
+		btnClearAlarm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				clockLogic.clearAlarm();
+				lblAlarmTime.setText(" No alarm");
+			}
+		});
+		btnClearAlarm.setBounds(377, 221, 117, 29);
+		contentPane.add(btnClearAlarm);
+		
+		textFieldMinutes = new JTextField();
+		textFieldMinutes.setBounds(183, 220, 60, 29);
+		contentPane.add(textFieldMinutes);
+		textFieldMinutes.setColumns(10);
+		
+		txtFieldHour = new JTextField();
+		txtFieldHour.setColumns(10);
+		txtFieldHour.setBounds(43, 220, 60, 29);
+		contentPane.add(txtFieldHour);
+		
+		lblCurrentTime.setBounds(393, 19, 101, 36);
+		contentPane.add(lblCurrentTime);
+		
+		lblAlarmTime.setBounds(393, 77, 101, 36);
+		contentPane.add(lblAlarmTime);
+		
+		JLabel lblNewLabel_1 = new JLabel("Hour");
+		lblNewLabel_1.setBounds(6, 226, 61, 16);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblMinutes = new JLabel("Minutes");
+		lblMinutes.setBounds(133, 226, 61, 16);
+		contentPane.add(lblMinutes);
+		
+		lblNewLabel.setIcon(new ImageIcon(DigitalClockGUI.class.getResource("/resources/1363846.jpg")));
+		lblNewLabel.setBounds(0, 0, 519, 277);
+		contentPane.add(lblNewLabel);
 	}
-	public void setTimeOnLabel( String time){
-	
+	public void setTimeOnLabel(String time) {
+		lblCurrentTime.setText(time);
 	}
 	
 	public void activateAlarm (boolean activate){
+		if(activate){
+			Toolkit.getDefaultToolkit().beep();
+			lblNewLabel.setIcon(new ImageIcon(DigitalClockGUI.class.getResource("/resources/vulkan.jpg")));
+
+		} else {
+			lblNewLabel.setIcon(new ImageIcon(DigitalClockGUI.class.getResource("/resources/1363846.jpg")));
+		}
 		
 	}
-
 }
